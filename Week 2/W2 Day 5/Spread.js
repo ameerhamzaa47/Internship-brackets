4c99a7b30e831bc73648796eb76ff274f52f6390
Ameer Hamza <hamzaataariq12@gmail.com>
2024-10-25 11:28:44 +0500

    initial state

--- Week 2/W2 Day 5/Spread.js
+++ Week 2/W2 Day 5/Spread.js
@@ -1,4 +1,13 @@
 "use strict";
-let arr1 = [1, 2, 3];
-let arr2 = [4, 5, 6];
-console.log(...arr1, ...arr2);
+const numbers = [1, 2, 3, 4, 5];
+let sum = 0;
+for (let i = 1; i <= numbers.length; i++) {
+    sum += i;
+}
+console.log(sum);
+let newNumbers = [...numbers, 6, 7];
+console.log("New array with additional elements:", newNumbers);
+const person = { name: 'Hamza', age: 20 };
+const contactInfo = { email: 'hamza@123' };
+const fullInfo = { ...person, ...contactInfo };
+console.log('mergeObject is', fullInfo);
--- Week 2/W2 Day 5/Spread.ts
+++ Week 2/W2 Day 5/Spread.ts
@@ -1,5 +1,17 @@
-let arr1=[1,2,3]
-let arr2=[4,5,6]
+const numbers:number[] = [1,2,3,4,5]
+let sum:number = 0
 
-console.log(...arr1,...arr2);
-console.log('66');
+for(let i=1;i<=numbers.length;i++){
+    sum+=i
+}
+
+console.log(sum);
+
+let newNumbers:number[]=[...numbers,6,7]
+console.log("New array with additional elements:", newNumbers);
+
+const person={name:'Hamza',age:20}
+const contactInfo={email:'hamza@123'}
+
+const fullInfo={...person,...contactInfo}
+console.log('mergeObject is',fullInfo);
